f01791c004159858d08d34d38f62decf1fd15e3f
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-07 15:58:53 +0200

    Check connect cam with client

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,6 +1,10 @@
-import React from "react";
+import {} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({}));
 
 const Options = ({ children }) => {
+  const classes = useStyles();
   return (
     <div>
       <p>Options</p>
