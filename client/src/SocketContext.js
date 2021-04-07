8aed948a50219eb7846233cc99a93677cb3786ee
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-07 15:04:42 +0200

    Add leavecall

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -77,5 +77,11 @@ const ContextProvider = ({ children }) => {
     connectionRef.current = peer;
   };
 
-  const leaveCall = () => {};
+  const leaveCall = () => {
+    setCallEnded(true);
+
+    connectionRef.current.destroy();
+
+    window.location.reload();
+  };
 };
