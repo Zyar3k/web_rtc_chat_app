b7dce4b6bb16d7fdda0d32a64cc8d29ac78e7fb3
Arkadiusz Zygan <a.zygan@gmail.com>
2021-04-07 14:43:28 +0200

    Add answercall

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -7,11 +7,15 @@ const SocketContext = createContext();
 const socket = io("http://localhost:5000");
 
 const ContextProvider = ({ children }) => {
-  const [stream, setStream] = useState(null);
+  const [stream, setStream] = useState();
   const [me, setMe] = useState("");
   const [call, setCall] = useState({});
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [callEnded, setCallEnded] = useState(false);
 
   const myVideo = useRef();
+  const userVideo = useRef();
+  const connectionRef = useRef();
 
   useEffect(() => {
     navigator.mediaDevices
@@ -29,7 +33,23 @@ const ContextProvider = ({ children }) => {
     });
   }, []);
 
-  const answerCall = () => {};
+  const answerCall = () => {
+    setCallAccepted(true);
+
+    const peer = new Peer({ initiator: false, trickle: false, stream });
+
+    peer.on("signal", (data) => {
+      socket.emit("answercall", { signal: data, to: call.from });
+    });
+
+    peer.on("stream", (currentStream) => {
+      userVideo.current.srcObject = currentStream;
+    });
+
+    peer.signal(call.signal);
+
+    connectionRef.current = peer;
+  };
 
   const callUser = () => {};
 
